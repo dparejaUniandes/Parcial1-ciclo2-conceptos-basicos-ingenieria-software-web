c15cb905e8e604f43219e0b25d9b423ef52a3fbc
dparejaUniandes <d.pareja@uniandes.edu.co>
2023-11-03 22:31:14 -0500

    Avanzo con la vista, falta definir bien el titulo y las divisiones

--- src/app/cafe/cafe-lista/coffee-list.component.ts
+++ src/app/cafe/cafe-lista/coffee-list.component.ts
@@ -10,6 +10,10 @@ import { CafeService } from '../cafe.service';
 export class CafeListarComponent implements OnInit {
 
   public cafes : Array<Cafe> = [];
+  public totalCafeOrigen : number = 0;
+  public totalCafeBlend : number = 0;
+  public readonly blend : string = "Blend"
+  public readonly cafeOrigen : string = "Café de Origen"
 
   constructor(private cafeService: CafeService) { }
 
@@ -20,7 +24,13 @@ export class CafeListarComponent implements OnInit {
   public getCafes() : void {
      this.cafeService.getCafes().subscribe(cafes => {
       this.cafes = cafes;
+      this.totalCafeOrigen = this.sumarCafesPorTipo(this.cafeOrigen)
+      this.totalCafeBlend = this.sumarCafesPorTipo(this.blend)
      })
+  }
+
+  private sumarCafesPorTipo(tipoCafe : string) : number {
+    return this.cafes.filter( cafe => cafe.tipo === tipoCafe).reduce((cantidad, ) => cantidad+1, 0)
   }
 
 }
