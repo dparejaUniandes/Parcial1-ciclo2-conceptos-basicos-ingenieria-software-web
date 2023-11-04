63e27a70f3d2f48f488a75fe6985c3ce1ac52088
dparejaUniandes <d.pareja@uniandes.edu.co>
2023-11-04 10:04:19 -0500

    Avanzo con las prueas del componente listar

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { CafeListarComponent } from './cafe/cafe-lista/cafe-lista.component';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    imports: [RouterTestingModule, HttpClientModule],
+    declarations: [AppComponent, CafeListarComponent]
   }));
 
   it('should create the app', () => {
@@ -18,12 +20,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Parcial');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Parcial app is running!');
   });
 });
--- src/app/cafe/cafe-lista/cafe-lista.component.spec.ts
+++ src/app/cafe/cafe-lista/cafe-lista.component.spec.ts
@@ -2,16 +2,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { faker } from '@faker-js/faker';
 
 import { CafeListarComponent } from './cafe-lista.component';
+import { CafeService } from '../cafe.service';
+import { Cafe } from '../cafe';
 
 describe('CafeListarComponent', () => {
   let component: CafeListarComponent;
   let fixture: ComponentFixture<CafeListarComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CafeListarComponent ]
+      imports: [HttpClientModule],
+      declarations: [ CafeListarComponent ],
+      providers: [CafeService]
     })
     .compileComponents();
   }));
@@ -19,10 +26,32 @@ describe('CafeListarComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CafeListarComponent);
     component = fixture.componentInstance;
+
+    for(let i = 0; i < 3; i++) {
+      const cafe = new Cafe(
+          faker.datatype.number(),
+          faker.lorem.sentence(),
+          faker.lorem.sentence(),
+          faker.lorem.sentence(),
+          faker.lorem.sentence(),
+          faker.datatype.number(),
+          faker.image.imageUrl(),
+        )
+        component.cafes.push(cafe)
+      }
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have 4 <tr> elements', () => {
+    expect(debug.queryAll(By.css('tr'))).toHaveSize(4)
+  });
+
+  it('should have 3 <td.fw-bold> elements', () => {
+    expect(debug.queryAll(By.css('td.fw-bold'))).toHaveSize(3)
   });
 });
